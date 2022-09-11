4ca601ad759abbde4b654a4377adc3f3281fe006
suhasnidgundi <suhasnidgundi@gmail..com>
2022-09-11 22:49:50 +0530

    Tutorial 4 (Arithmetic Operator)

---
+++ Tutorial 4/main.js
@@ -0,0 +1,27 @@
+// Practical No 1: Write simple JavaScript with HTML for arithmetic expression evaluation and message printing.
+
+var a = 10;
+var b = 20;
+
+document.write("Addition : ", a+b);
+document.write("<br>");
+
+document.write("Subtraction : ", a-b);
+document.write("<br>");
+
+document.write("Multiplication : ", a*b);
+document.write("<br>");
+
+document.write("Exponentiation : ", a**b);
+document.write("<br>");
+
+document.write("Division : ", a/b);
+document.write("<br>");
+
+document.write("Modulus : ", a%b);
+document.write("<br>");
+
+document.write("Increment : ", a++);
+document.write("<br>");
+
+document.write("Decrement : ", b--);
